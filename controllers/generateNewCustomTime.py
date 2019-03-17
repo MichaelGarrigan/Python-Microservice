54e08b0f157ba53d2cfdc662294a2f2094dbfdc3
M-garrigan <mgarrigan99@gmail.com>
2019-03-17 18:20:50 -0400

    Filled out the db model function that retrieves the old timestamp.
    This time stamp is held in a db table called last_custom_time_stamp and
    only has one row, this row will continuously be updated every five minutes.
    The row holds an array of 5 integers that holds to this pattern:
    [day, month, year, minute, hour] and an example would be [17, 3, 2019, 0, 13].
    Please note that hours will be in military format.

--- controllers/generateNewCustomTime.py
+++ controllers/generateNewCustomTime.py
@@ -1,8 +1,16 @@
 
+from models.customTimeModel import retrieveOldCustomTime
+
 # connect to db
+def newCustomTime(dbConnection, cursor):
 
-# query db for last time recorded
+  # query db for last time recorded
+  oldTime = retrieveOldCustomTime(dbConnection, cursor)
+  oldTimeAsAList = oldTime[0]
 
-# increment time and save to a variable
+  print('oldTimeList', oldTimeAsAList)
+  
+  
+  # increment time and save to a variable
 
-# write new time to db
+  # write new time to db
--- models/customTimeModel.py
+++ models/customTimeModel.py
@@ -1,6 +1,24 @@
 
-# expose two functions
+'''
+@name -> retrieveOldCustomTime 
 
-# 1) retrieve old time from last_custom_time_stamp table
+@param (dbConnection) -> db connection object
+@param (cursor) -> db cursor object
+@return -> a python tuple with a single list of 5 integers
+
+@about -> This model will only retrieve the same row from the same table with every call.
+       -> The column 'time_custom' will have a value that constantly changes every five minutes.
+'''
+
+def retrieveOldCustomTime(dbConnection, cursor):
+  sql = 'SELECT time_custom FROM last_custom_time_stamp WHERE id=1'
+  cursor.execute(sql)
+  return cursor.fetchone()
 
 # 2) insert new time into last_custom_time_stamp table
+
+# query = """ 
+#       INSERT INTO top_games (GAME_ID, NAME, BOX_ART_URL) VALUES (%s,%s,%s)"""
+#     values = (item['id'], item['name'], item['box_art_url'])
+#     cursor.execute(query, values)
+#     connection.commit()
