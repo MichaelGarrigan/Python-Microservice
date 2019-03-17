c139a79ba0f6b207bb0c22427c9cb66abfaa5621
M-garrigan <mgarrigan99@gmail.com>
2019-03-17 12:37:39 -0400

    Added if-else clause around dbConnection variable.
    The idea is to return an error or the connection object. If we get an error we want to write a custom log
    entry into our logger which is not set up yet but will most likely be a file in an AWS S3 bucket.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 
+from dbConnection import connectToDB
+import psycopg2
 
 # connect to postgres db
+dbConnection = connectToDB()
 
-# Generate Custom Time with /controllers/generateNewCustomTime.py
+if type(dbConnection) == psycopg2.OperationalError:
+  # log out error message and timestamp
+  print('DB error: ', dbConnection)
+else:
+  cursor = dbConnection.cursor()
 
-# Get top games from twitch api and insert into db
-# => /controllers/apiTopGames.py
+  # Generate Custom Time with /controllers/generateNewCustomTime.py
 
-# Get top streamers from twitch api and insert into db
-# => /controllers/apiTopStreamers.py
+  # Get top games from twitch api and insert into db
+  # => /controllers/apiTopGames.py
 
-# disconnect from postgres db
+  # Get top streamers from twitch api and insert into db
+  # => /controllers/apiTopStreamers.py
+
+  # disconnect from postgres db
+  cursor.close()
+  dbConnection.close()
+  print("PostgreSQL connection is closed")
