1618592885497cb58ba10370403b7e6e8b19adae
M-garrigan <mgarrigan99@gmail.com>
2019-03-17 18:18:15 -0400

    Wrapped previous code in a handle_main function.
    This is so that when the lambda service calls every five minutes it will have a traditional
    'entry point'. Also there is an invocation below this function that can be uncommented in order
    to call handle_main locally.

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 
 from dbConnection import connectToDB
+from controllers.generateNewCustomTime import newCustomTime
 import psycopg2
+import datetime
 
-# connect to postgres db
-dbConnection = connectToDB()
+# Entry Point for AWS Lambda Service
+def handle_main(event, context):
 
-if type(dbConnection) == psycopg2.OperationalError:
-  # log out error message and timestamp
-  print('DB error: ', dbConnection)
-else:
-  cursor = dbConnection.cursor()
+  # connect to postgres db
+  dbConnection = connectToDB()
 
-  # Generate Custom Time with /controllers/generateNewCustomTime.py
+  if type(dbConnection) == psycopg2.OperationalError:
+    # --TODO-- log out error message and timestamp
+    print('DB error: ', dbConnection)
+  else:
+    cursor = dbConnection.cursor()
+    
+    # Generate Custom Time with /controllers/generateNewCustomTime.py
+    print('select: ', newCustomTime(dbConnection, cursor))
+    # --TODO-- capture this in a variable and sent to a new function to process it
 
-  # Get top games from twitch api and insert into db
-  # => /controllers/apiTopGames.py
+    # Get top games from twitch api and insert into db
+    # => /controllers/apiTopGames.py
 
-  # Get top streamers from twitch api and insert into db
-  # => /controllers/apiTopStreamers.py
+    # Get top streamers from twitch api and insert into db
+    # => /controllers/apiTopStreamers.py
 
-  # disconnect from postgres db
-  cursor.close()
-  dbConnection.close()
-  print("PostgreSQL connection is closed")
+    # disconnect from postgres db
+    cursor.close()
+    dbConnection.close()
+    print("PostgreSQL connection is closed")
+
+# Uncomment to run this project locally
+handle_main('event', 'context')
