63d11e8dd90459541f5e899d69d18d495ec9a50f
M-garrigan <mgarrigan99@gmail.com>
2019-03-29 15:21:51 -0400

    Change api limit to accept 20 items.

--- microservice/src/controllers/apiTopGames.py
+++ microservice/src/controllers/apiTopGames.py
@@ -6,28 +6,25 @@ from models.topGamesStaticModel import selectGameIfAvailable, insertGameIntoStat
 from helperFunctions.config import twitch
 
 def callTwitchApi():
-    url = 'https://api.twitch.tv/helix/games/top'
-    payload = {'first': '16'}
+    url = 'https://api.twitch.tv/kraken/games/top'
+    payload = {'limit': '21'}
     headers = {'Client-ID': twitch['Client-ID']}
 
     res = requests.get(url, params=payload, headers=headers)
 
     # parse from json
     topGames = json.loads(res.text)
-    topGamesList = topGames['data']
-
-    print('top games: ', len(topGamesList))
+    topGamesList = topGames['top']
 
     return topGamesList
 
 def retrieveTopGames(dbConnection, cursor, time):
-
   games = callTwitchApi()
-
+  
   # iterate over games
   for game in games:
     # staticGame will be either None or a tuple
-    staticGame = selectGameIfAvailable(dbConnection, cursor, int(game['id']))
+    staticGame = selectGameIfAvailable(dbConnection, cursor, int(game['game']['_id']))
     if staticGame is None: 
       insertGameIntoStaticTable(dbConnection, cursor, game)
       
