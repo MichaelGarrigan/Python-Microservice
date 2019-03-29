fe552d29d48a594f5f0307644613744cb9ba4f2c
M-garrigan <mgarrigan99@gmail.com>
2019-03-29 13:39:39 -0400

    Add the portion of the model to handle dynamic values.

--- microservice/src/models/topGamesStaticModel.py
+++ microservice/src/models/topGamesStaticModel.py
@@ -6,13 +6,15 @@ def selectGameIfAvailable(dbConnection, cursor, id):
   query = sql.SQL('SELECT * FROM top_games_static WHERE game_id={}').format(sql.Literal(id))
   cursor.execute(query)
   gameTuple = cursor.fetchone()
-  print('static return: ', gameTuple)
-  print('static return type: ', type(gameTuple))
+
   return gameTuple
 
 
 # 2) insert new game into top_games_static table
 def insertGameIntoStaticTable(dbConnection, cursor, game):
   query = 'INSERT INTO top_games_static (game_id, name, box_art_url, logo_template) VALUES (%s, %s, %s, %s)'
-  cursor.execute(query, [game['id'], game['name'], game['box_art_url'], 'none'])
+  cursor.execute(
+    query, 
+    [game['game']['_id'], game['game']['name'], game['game']['box']['template'], game['game']['logo']['template']]
+  )
   dbConnection.commit()
