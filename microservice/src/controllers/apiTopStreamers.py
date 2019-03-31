e58e03617caca924619bde7588b01689144f94af
M-garrigan <mgarrigan99@gmail.com>
2019-03-31 07:26:15 -0400

    Wrote a controller to handle our top streamers.
    There are two functions:
    1) callTwitchApi - retrieves data from our /streams endpoint
    2) retrieveTopStreamers - parses data from api call and iterates over them
    to see if this streamer is already in the top_streams_static table and will
    enter a row if it is not present. Then we write the data to the top_streams table.

--- microservice/src/controllers/apiTopStreamers.py
+++ microservice/src/controllers/apiTopStreamers.py
@@ -1,8 +1,31 @@
 
-# call twitch api for current top streams data
+import requests
+import json
+from models.streamersModel import insertTopStreams
+from models.streamersStaticModel import selectStreamIfAvailable, insertStreamIntoStaticTable
+from helperFunctions.config import twitch
 
-# check to see if any new data needs to be added to streamers_static table
-# use models/streamersStaticModel.py
-# => if yes add it to that table
+def callTwitchApi():
+    url = 'https://api.twitch.tv/kraken/streams'
+    headers = {'Client-ID': twitch['Client-ID']}
 
-# then add data to db with models/streamersModel.py
+    res = requests.get(url, headers=headers)
+    # parse from json
+    topStreams = json.loads(res.text)
+    topStreamsList = topStreams['streams']
+
+    return topStreamsList
+
+def retrieveTopStreamers(dbConnection, cursor, time):
+  streams = callTwitchApi()
+  
+  # iterate over streams
+  for stream in streams:
+    # staticStreams will be either None or a tuple
+    staticStreams = selectStreamIfAvailable(dbConnection, cursor, int(stream['channel']['_id']))
+    if staticStreams is None: 
+      insertStreamIntoStaticTable(dbConnection, cursor, stream)
+      
+
+  # then add data to db with models/topStreamsModel.py
+  insertTopStreams(dbConnection, cursor, time, streams) 
